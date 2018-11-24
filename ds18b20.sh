620e7e5efb9bfbfc8d45333021f65ab9829a4482
pi <pi@raspberrypi.(none)>
2018-11-24 13:21:29 +0000

    /boot/config.txt

--- ds18b20.sh
+++ ds18b20.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-t1=`cat /sys/bus/w1/devices/28-000008a70c84/w1_slave | grep -oP 't=\K.+'`
-t2=`cat /sys/bus/w1/devices/28-000008a719a8/w1_slave | grep -oP 't=\K.+'`
-t3=`cat /sys/bus/w1/devices/28-000008a8d67d/w1_slave | grep -oP 't=\K.+'`
+#Add dtoverlay=w1-gpio,gpiopin=4 in /boot/config.txt before using
+t1=`cat /sys/bus/w1/devices/28-000008a*****/w1_slave | grep -oP 't=\K.+'`
+t2=`cat /sys/bus/w1/devices/28-000008a*****/w1_slave | grep -oP 't=\K.+'`
+t3=`cat /sys/bus/w1/devices/28-000008a*****/w1_slave | grep -oP 't=\K.+'`
