844670c42b61d6e2c90a9fb545a0bb0eb89a31ad
pi <pi@raspberrypi.(none)>
2018-12-09 00:21:42 +0200

    lan Influx

--- cpu_gpu.sh
+++ cpu_gpu.sh
@@ -7,3 +7,6 @@ echo $cpu_temp
 
 gpu_temp=`/opt/vc/bin/vcgencmd measure_temp | grep -o -P '=\K.{0,4}+'`
 echo $gpu_temp
+
+curl -i -XPOST 'http://ip:8086/write?db=influx' --data-binary "cpu_temp,host=pi value=$cpu_temp
+gpu_temp,host=pi value=$gpu_temp"
