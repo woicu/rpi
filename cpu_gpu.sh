4333dd8b14ed9e21856921967db5f5ef32fcd273
test <test>
2019-02-10 12:21:49 +0000

    Localhost added

--- cpu_gpu.sh
+++ cpu_gpu.sh
@@ -8,5 +8,5 @@ echo $cpu_temp
 gpu_temp=`/opt/vc/bin/vcgencmd measure_temp | grep -o -P '=\K.{0,4}+'`
 echo $gpu_temp
 
-curl -i -XPOST 'http://ip:8086/write?db=influx' --data-binary "cpu_temp,host=pi value=$cpu_temp
+curl -i -XPOST 'http://localhost:8086/write?db=influx' --data-binary "cpu_temp,host=pi value=$cpu_temp
 gpu_temp,host=pi value=$gpu_temp"
