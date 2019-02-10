ef3d0a47e7025ee6b4cbb8c7e207ff656aba16ff
test <test>
2019-02-10 11:58:37 +0000

    localhost added

--- ds18b20.sh
+++ ds18b20.sh
@@ -4,3 +4,7 @@
 t1=`cat /sys/bus/w1/devices/28-000008a*****/w1_slave | grep -oP 't=\K.+'`
 t2=`cat /sys/bus/w1/devices/28-000008a*****/w1_slave | grep -oP 't=\K.+'`
 t3=`cat /sys/bus/w1/devices/28-000008a*****/w1_slave | grep -oP 't=\K.+'`
+
+curl -i -XPOST 'http://localhost:8086/write?db=telegraf' --data-binary "T1,host=pi value=$t1
+T2,host=pi value=$t2
+T3,host=pi value=$t3"
