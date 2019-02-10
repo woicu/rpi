e582855dee09fcbf485bd336445c6ab0f0e1d2a5
test <test>
2019-02-10 11:00:08 +0000

    rm stack

--- tick.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Nohup to avoid getting stuck as suggested in https://github.com/moby/moby/issues/438#issuecomment-262188806
-time nohup docker pull arm32v7/telegraf:latest
-time nohup docker pull arm32v7/influxdb:latest
-time nohup docker pull arm32v7/chronograf:latest
-
-rm ./nohup.out
